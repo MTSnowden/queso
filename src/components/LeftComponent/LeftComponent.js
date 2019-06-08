871eecb0207ad9d7180ceac33fa5a92e5e5f668e
Michael Snowden <mtsnowden@gmail.com>
2019-06-07 19:35:32 -0500

    fleshing out components

--- src/components/LeftComponent/LeftComponent.js
+++ src/components/LeftComponent/LeftComponent.js
@@ -5,6 +5,9 @@ const LeftComponent = () => (
   <div className="LeftComponent">
     <div className="left-flex">
       <h1>This is a map of austin </h1>
+      <p>The map will have a search bar on the top</p>
+      <p>Will generate all cheese dishes in the area</p>
+      <p>Will generate icons for cheez kweens</p>
     </div>
     
   </div>
