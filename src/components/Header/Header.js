878e7bffa64148279c8666e092f039301cf8aa13
Michael Snowden <mtsnowden@gmail.com>
2019-06-13 16:54:50 -0500

    adding logo

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -6,20 +6,20 @@ const Header = () => (
   <div className="header">
 
     <div className="header-flex">
-    <img className="logo" src="../images/logo" alt="logo"></img>
+      <img id="logo" alt="logo"></img>
     </div>
 
     <div className="header-flex">
       <div>
-      <h1>This is the header.</h1>
+      <h1>CHZPLZ</h1>
       </div>
     </div>
 
-      <div className="header-flex">
-        <div>
+    <div className="header-flex">
+      <div>
         <button>account login button</button>
-        </div>
       </div>
+    </div>
     
   </div>
 )
