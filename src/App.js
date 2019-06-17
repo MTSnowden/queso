3dba879c14965db2ef176f4130a9c74301460043
Michael Snowden <mtsnowden@gmail.com>
2019-06-17 15:15:50 -0500

    working on nav

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
 import React, { Component } from 'react';
-import Header from './components/Header/Header';
+// import Header from './components/Header/Header';
 import './App.css';
-import {BrowserRouter} from 'react-router-dom';
-import Router from './Router';
+import "bootstrap/dist/css/bootstrap.min.css"
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+// import Router from './Router';
+import logo from '../src/logo.png'
 import LeftComponent from './components/LeftComponent/LeftComponent';
 import RightComponent from './components/RightComponent/RightComponent';
 
 function App() {
   return (
-    <div className="App">
-      <BrowserRouter>
-      <Header />
-      <Router />
-      </BrowserRouter>
-    <div className="body">
+    
+    <Router>
+    <div className="container">
+      <nav className="navbar navbar-expand-lg navbar-light">
+
+        <a className="navbar-brand">
+          <img src={logo} width="50" height="50" alt="logo" />
+        </a>
+
+        <Link to ="/" className="navbar-brand">CHZPLZ</Link>
+        <div className="collapse nav-collapse">
+          <ul className="navbar nav mr-auto">
+            <li className="navbar-item">
+              {/* <Link to="" */}
+            </li>
+            <li className="navbar-item">
+              {/* <Link to="" */}
+            </li>
+          </ul>
+        </div>
+      </nav>
+
+      <div className="body">
        <LeftComponent />
        <RightComponent />
       </div>
+
     </div>
+    </Router>
   );
 }
 
 export default App;
-
